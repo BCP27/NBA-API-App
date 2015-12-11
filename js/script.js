f2a3cf1fbc07472cd6d1e07603f77c9631d3a94c
BCP27 <gamemeister27@gmail.com>
2015-12-11 03:05:03 -0600

    Fixed data formatting, first dropdown list populating properly

--- js/script.js
+++ js/script.js
@@ -3,23 +3,30 @@ $(document).ready(function () {
     var DropDown = $("<select>");
     var WorkSpace = $("#menu");
     
+    DropDown.attr("id", "name");
     WorkSpace.append(DropDown);
+    
+    $("<select>").attr("id", "perunit");
+    WorkSpace.append($("<select>").attr("id", "perunit"));
+    
     
     
     function populate() {
         console.log("test");
         $.ajax({
             url: "data/Team_constants.json",
-            
-            datatype: "json",
      
             success: function (data) {
                 console.log("test");
-                for (i = 0; i < data.length; i = i + 1) {
+                console.log(data);
+                console.log(data["NBA Teams"][0]["abbr"]);
+                var i;
+                var nba = data["NBA Teams"];
+                for (i = 0; i < nba.length; i = i + 1) {
                     var TeamName = $("<option>");
-                    TeamName.attr("value", data[i].abbr);
-                    TeamName.text(data[i].abbr);
-                    DropDown.append(TeamName);
+                    TeamName.attr("value", nba[i]["abbr"]);
+                    TeamName.text(nba[i]["abbr"]);
+                    $("#name").append(TeamName);
                 }
             }
         });
